344c8b76e5625b6a05a55d47a97ba2072f95c48a
jeonghyeon Hwang <hijihilin@gmail.com>
2022-10-18 19:47:37 +0900

    [chore] 프로젝트 파일 생성

---
+++ OrrRock/OrrRock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrrRock
+//
+//  Created by 황정현 on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
