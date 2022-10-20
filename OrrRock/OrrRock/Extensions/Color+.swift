005e1baa17c891bc334e66e4c225199f10306d17
RuyHa <code.ruyha@gmail.com>
2022-10-20 16:17:32 +0900

    [#8] feat - 앱 컬러 Setting

---
+++ OrrRock/OrrRock/Extensions/Color+.swift
@@ -0,0 +1,25 @@
+//
+//  Color+.swift
+//  OrrRock
+//
+//  Created by Ruyha on 2022/10/20.
+//
+
+import UIKit
+
+extension UIColor{
+    //MAIN COLOR
+    static let oRUPBlue = UIColor(named: "oRUPBlue")
+
+    //GRAY COLOR
+    static let oRBlack = UIColor(named: "oRBlack")
+    static let oRWhite = UIColor(named: "oRWhite")
+    static let oRGray1 = UIColor(named: "oRGray1")
+    static let oRGray3 = UIColor(named: "oRGray3")
+    static let oRGray4 = UIColor(named: "oRGray4")
+    static let oRGray5 = UIColor(named: "oRGray5")
+    
+    //SUB COLOR
+    static let oRPass = UIColor(named: "oRPass")
+    static let oRFail = UIColor(named: "oRFail")
+}
