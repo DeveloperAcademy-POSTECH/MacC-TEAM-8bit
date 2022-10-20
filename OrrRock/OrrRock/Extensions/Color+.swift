86d2b7b4b7f6844f1a4a1edc50543c19cfcf2530
RuyHa <code.ruyha@gmail.com>
2022-10-20 16:48:28 +0900

    [#8] feat - 폰트이름 변경

--- OrrRock/OrrRock/Extensions/Color+.swift
+++ OrrRock/OrrRock/Extensions/Color+.swift
@@ -9,17 +9,17 @@ import UIKit
 
 extension UIColor{
     //MAIN COLOR
-    static let oRUPBlue = UIColor(named: "oRUPBlue")
+    static let orrUPBlue = UIColor(named: "oRUPBlue")
 
     //GRAY COLOR
-    static let oRBlack = UIColor(named: "oRBlack")
-    static let oRWhite = UIColor(named: "oRWhite")
-    static let oRGray1 = UIColor(named: "oRGray1")
-    static let oRGray3 = UIColor(named: "oRGray3")
-    static let oRGray4 = UIColor(named: "oRGray4")
-    static let oRGray5 = UIColor(named: "oRGray5")
+    static let orrBlack = UIColor(named: "oRBlack")
+    static let orrWhite = UIColor(named: "oRWhite")
+    static let orrGray1 = UIColor(named: "oRGray1")
+    static let orrGray3 = UIColor(named: "oRGray3")
+    static let orrGray4 = UIColor(named: "oRGray4")
+    static let orrGray5 = UIColor(named: "oRGray5")
     
     //SUB COLOR
-    static let oRPass = UIColor(named: "oRPass")
-    static let oRFail = UIColor(named: "oRFail")
+    static let orrPass = UIColor(named: "oRPass")
+    static let orrFail = UIColor(named: "oRFail")
 }
