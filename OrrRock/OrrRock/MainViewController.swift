9e90905d4bc7e5bb18dc7b75026d850967a935dd
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-10-20 16:33:37 +0900

    [#11] feat - 테스트버튼 레이아웃 설정

--- OrrRock/OrrRock/MainViewController.swift
+++ OrrRock/OrrRock/MainViewController.swift
@@ -6,18 +6,23 @@
 //
 
 import UIKit
-
+import SnapKit
 class MainViewController: UIViewController {
     
     private lazy var testButton : UIButton = {
         let button = UIButton()
         button.setTitle("gotoalbum", for: .normal)
+        button.tintColor = .blue
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        view.addSubview(testButton)
+        testButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
     }
 
 
