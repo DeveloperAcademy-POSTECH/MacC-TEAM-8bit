9185447872bf81d9b64227ddd796305b6a1129a7
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-10-21 06:44:37 +0900

    [#11] feat - BasePaddingLabel 추가

--- OrrRock/OrrRock/ViewControllers/VideoCollectionViewController/VideoCollectionViewController.swift
+++ OrrRock/OrrRock/ViewControllers/VideoCollectionViewController/VideoCollectionViewController.swift
@@ -11,7 +11,8 @@ class VideoCollectionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        
         // Do any additional setup after loading the view.
     }
     
--- OrrRock/OrrRock/Views/VideoCollectionView/VideoCollectionViewCustomCell.swift
+++ OrrRock/OrrRock/Views/VideoCollectionView/VideoCollectionViewCustomCell.swift
@@ -5,4 +5,30 @@
 //  Created by dohankim on 2022/10/20.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class VideoCollectionViewCell : UICollectionViewCell{
+    static let identifier = "customVideoCollectionCell"
+    
+    private lazy var cellImage : UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.image = UIImage(named: "")
+        return
+    }()
+    
+    private lazy var cellLabel : UILabel = {
+        let label = BasePaddingLabel(padding: UIEdgeInsets(top: 2, left: 8, bottom: 2, right: 8))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "V2"
+        label.font = UIFont.systemFont(ofSize: 10, weight: .bold)
+        label.backgroundColor = .red
+        label.textColor = .white
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 8
+        return label
+    }()
+    
+    
+}
