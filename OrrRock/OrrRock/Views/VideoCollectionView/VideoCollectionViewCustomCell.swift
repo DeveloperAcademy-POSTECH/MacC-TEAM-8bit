4e3f0b3d375229743e5adbb043eeeaf662f40843
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-10-21 22:32:59 +0900

    [#11] refactor - 필요한 공백 추가

--- OrrRock/OrrRock/Views/VideoCollectionView/VideoCollectionViewCustomCell.swift
+++ OrrRock/OrrRock/Views/VideoCollectionView/VideoCollectionViewCustomCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class VideoCollectionViewCell : UICollectionViewCell{
+class VideoCollectionViewCell : UICollectionViewCell {
     static let identifier = "customVideoCollectionCell"
     
     
