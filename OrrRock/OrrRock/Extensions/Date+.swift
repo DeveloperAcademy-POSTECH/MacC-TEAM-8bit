90bcfb0407b6e10377e7aa218f2200cc1d92418d
Hyeonmin Kim <hminkim2@gmail.com>
2022-10-22 19:51:34 +0900

    [#35] feat - Date to String 함수 생성

---
+++ OrrRock/OrrRock/Extensions/Date+.swift
@@ -0,0 +1,19 @@
+//
+//  Date+.swift
+//  OrrRock
+//
+//  Created by kimhyeongmin on 2022/10/22.
+//
+
+import Foundation
+
+extension Date {
+	//MARK: Date -> String 타입 변환 메서드
+	func timeToStringYMD() -> String { // "yyyy년 M월 d일"
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "yyyy년 M월 d일"
+		dateFormatter.locale = Locale(identifier:"ko_KR")
+		
+		return dateFormatter.string(from: self)
+	}
+}
