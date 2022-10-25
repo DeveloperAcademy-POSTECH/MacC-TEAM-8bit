d2384170b2cd01da03bea152ea7f418f3c50b856
Hyeonmin Kim <hminkim2@gmail.com>
2022-10-25 21:03:45 +0900

    [#55] fix - 텍스트 색상 에셋 색상으로 변경

--- OrrRock/OrrRock/Views/VideoDetailView/VideoInfoView.swift
+++ OrrRock/OrrRock/Views/VideoDetailView/VideoInfoView.swift
@@ -123,7 +123,7 @@ extension VideoInfoView: UITextViewDelegate {
 	// 텍스트뷰가 비어있을 때 플레이스 홀더 띄워주는 메서드
 	func textViewDidBeginEditing(_ textView: UITextView) {
 		guard textView.textColor == .placeholderText else { return }
-		textView.textColor = .label
+		textView.textColor = .orrBlack
 		textView.text = nil
 	}
 
