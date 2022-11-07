ee714628c9e0d32505ff89f3e639e132335f3333
RuyHa <code.ruyha@gmail.com>
2022-11-07 21:58:27 +0900

    [fix] 홈화면 화질 개선 및 코드 오류 수정

--- OrrRock/OrrRock/ViewControllers/VideoCollectionViewController/VideoCollectionView+VideoCollectionViewExtensions.swift
+++ OrrRock/OrrRock/ViewControllers/VideoCollectionViewController/VideoCollectionView+VideoCollectionViewExtensions.swift
@@ -125,11 +125,11 @@ extension VideoCollectionViewController : UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 1
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = collectionView.frame.width / 3 - 1
         return CGSize(width: width, height: width * 2.13)
     }
-    
-    
+
+
 }
