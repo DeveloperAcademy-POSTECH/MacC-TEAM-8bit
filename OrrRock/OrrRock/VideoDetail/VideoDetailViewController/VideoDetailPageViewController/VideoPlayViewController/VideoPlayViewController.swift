cb07b07d93b9a457a11a8e2bd29a0a0152ecf900
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-11-20 20:19:36 +0900

    [#211] fix - pause구문 삭제

--- OrrRock/OrrRock/VideoDetail/VideoDetailViewController/VideoDetailPageViewController/VideoPlayViewController/VideoPlayViewController.swift
+++ OrrRock/OrrRock/VideoDetail/VideoDetailViewController/VideoDetailPageViewController/VideoPlayViewController/VideoPlayViewController.swift
@@ -101,7 +101,6 @@ extension VideoPlayViewController{
     
     override func viewWillDisappear(_ animated: Bool) {
         self.queuePlayer.isMuted = true
-        self.queuePlayer.pause()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
