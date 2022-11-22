08b2d05849d275603958db836541dae96de719d7
Hyeonmin Kim <hminkim2@gmail.com>
2022-11-23 07:38:05 +0900

    [#230] feat - export 모달뷰 exportViewController 생성

---
+++ OrrRock/OrrRock/VideoDetail/ExportViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ExportViewController.swift
+//  OrrRock
+//
+//  Created by kimhyeongmin on 2022/11/23.
+//
+
+import UIKit
+import Photos
+
+class ExportViewController: UIViewController {
+    
+    var videoInformation: VideoInformation!
+    var videoAsset: PHAsset?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orrWhite
+    }
+}
+    
