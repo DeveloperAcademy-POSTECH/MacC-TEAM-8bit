fe7f706a185bc67531a3a9384a7f1b2a6b5fc076
RuyHa <code.ruyha@gmail.com>
2022-11-22 21:03:26 +0900

    [#237] feat 쓸모없는 주석제거

--- OrrRock/OrrRock/SwipeOnboarding/SwipeOnboardingSecondViewController.swift
+++ OrrRock/OrrRock/SwipeOnboarding/SwipeOnboardingSecondViewController.swift
@@ -8,12 +8,6 @@
 import UIKit
 import SnapKit
 
-
-/* MARK: 추가 작업사항
-    1. 상단에 레벨 피커 추가
-    2. 레벨 피커 위의 문구는 화면 크기 혹은 홈버튼 유무에 따라 보이거나 안보이게 처리 해야함
- */
-
 class SwipeOnboardingSecondViewController: UIViewController {
     
     // 오토레이아웃의 시작점이 되는 값입니다. 변경시 류하에게 문의 주세요.
