8a3dd543d65d1f27a05093daf0a0a1f1dd66207a
Hyeonmin Kim <hminkim2@gmail.com>
2022-11-23 07:44:01 +0900

    [#230] feat - 모달뷰 위에 커스텀 네비게이션바 생성

--- OrrRock/OrrRock/VideoDetail/ExportViewController.swift
+++ OrrRock/OrrRock/VideoDetail/ExportViewController.swift
@@ -8,14 +8,39 @@
 import UIKit
 import Photos
 
-class ExportViewController: UIViewController {
+class ExportViewController: UIViewController, UINavigationBarDelegate {
     
     var videoInformation: VideoInformation!
     var videoAsset: PHAsset?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationBar()
         view.backgroundColor = .orrWhite
+    }
+    
+    func setNavigationBar() {
+        let navbar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 75))
+        navbar.backgroundColor = UIColor.orrWhite
+        navbar.delegate = self
+        
+        let navItem = UINavigationItem()
+        navItem.title = "사진에 저장"
+        navItem.leftBarButtonItem = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelAction))
+        navItem.rightBarButtonItem = UIBarButtonItem(title: "완료", style: .plain, target: self, action: #selector(completeAction))
+        
+        navbar.items = [navItem]
+        
+        view.addSubview(navbar)
+    }
+    
+    @objc private func completeAction() {
+        //TODO: 확인 버튼 후 다음 뷰 띄워주기
+        print(#function)
+    }
+    
+    @objc private func cancelAction() {
+        self.dismiss(animated: true)
     }
 }
     
