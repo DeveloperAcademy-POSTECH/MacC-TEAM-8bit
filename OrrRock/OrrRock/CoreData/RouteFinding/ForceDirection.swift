66a92a95fe4059b1a64f4793cab232742fe6b699
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-24 22:44:33 +0900

    [#223] feat - ForceDirection Radian 변환 메소드 추가

--- OrrRock/OrrRock/CoreData/RouteFinding/ForceDirection.swift
+++ OrrRock/OrrRock/CoreData/RouteFinding/ForceDirection.swift
@@ -9,4 +9,8 @@ import Foundation
 
 enum ForceDirection: Int {
     case pi0,pi1, pi2, pi3, pi4, pi5, pi6, pi7
+    
+    func forceDirectionToRadian() -> Double {
+        return Double((self.rawValue - 1) / 2) * .pi
+    }
 }
