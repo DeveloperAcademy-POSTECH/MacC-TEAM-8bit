ce684a11a35a8980f9ed1812b5286da27e73551a
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-11-24 17:07:05 +0900

    [#221] add routeFindingMainView

---
+++ OrrRock/OrrRock/RouteFinding/RouteFindingMainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrrRock
+//
+//  Created by dohankim on 2022/11/24.
+//
+
+import UIKit
+
+class RouteFindingMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
