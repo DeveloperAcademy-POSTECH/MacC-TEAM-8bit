38b4bbd2770c353ee27f6bc12dcb6d7c6174d89d
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-24 22:47:57 +0900

    [#223] feat - RouteFinding 관련 Draft Model 추가
    - RouteInfo
    - PageInfo
    - PointInfo

---
+++ OrrRock/OrrRock/CoreData/RouteFinding/PageInfo.swift
@@ -0,0 +1,13 @@
+//
+//  PageInfo.swift
+//  OrrRock
+//
+//  Created by 황정현 on 2022/11/24.
+//
+
+import Foundation
+
+struct PageInfo {
+    var rowOrder: Int
+    var points: [PointInfo]?
+}
---
+++ OrrRock/OrrRock/CoreData/RouteFinding/PointInfo.swift
@@ -0,0 +1,15 @@
+//
+//  PointInfo.swift
+//  OrrRock
+//
+//  Created by 황정현 on 2022/11/24.
+//
+
+import Foundation
+
+struct PointInfo: Equatable {
+    var footOrHand: FootOrHand
+    var isForce: Bool
+    var primaryPosition: CGPoint
+    var forceDirection: ForceDirection
+}
