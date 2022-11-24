d7d9648149b19f66f3f79820c98a87c306736335
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-11-24 20:18:40 +0900

    [#221] feat - add RouteFindingPageViewController

--- OrrRock/OrrRock/RouteFinding/RouteFindingMainViewController.swift
+++ OrrRock/OrrRock/RouteFinding/RouteFindingMainViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import SnapKit
 
 class RouteFindingMainViewController: UIViewController {
 
+    
+    lazy var topView : UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    lazy var titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "루트 파인딩"
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+    
+    lazy var plusButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "plus",withConfiguration: UIImage.SymbolConfiguration(pointSize: 22, weight: .bold)), for: .normal)
+        button.tintColor = .orrGray600
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpLayout()
+        setNavigationBar()
     }
     
-
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    func setUpLayout(){
+        view.backgroundColor = .orrGray100
+        view.addSubview(topView)
+        topView.snp.makeConstraints {
+            $0.top.equalTo(view.forLastBaselineLayout.snp_topMargin).offset(OrrPd.pd8.rawValue)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(26)
+        }
+        topView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(OrrPd.pd16.rawValue)
+        }
+        topView.addSubview(plusButton)
+        plusButton.snp.makeConstraints{
+            $0.trailing.equalToSuperview().offset(-OrrPd.pd16.rawValue)
+        }
+    }
+    
+    func setNavigationBar(){
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    
 }
---
+++ OrrRock/OrrRock/RouteFinding/RouteFindingPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RouteFindingPageViewController.swift
+//  OrrRock
+//
+//  Created by dohankim on 2022/11/24.
+//
+
+import UIKit
+
+class RouteFindingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
