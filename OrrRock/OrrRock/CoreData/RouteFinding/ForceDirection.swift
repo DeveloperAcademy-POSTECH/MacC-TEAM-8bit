5df6f14d2549b5a59a76ddd6084fba99259d3e3c
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-24 22:37:24 +0900

    [#223] feat - enum ForceDirection 추가

---
+++ OrrRock/OrrRock/CoreData/RouteFinding/ForceDirection.swift
@@ -0,0 +1,12 @@
+//
+//  ForceDirection.swift
+//  OrrRock
+//
+//  Created by 황정현 on 2022/11/24.
+//
+
+import Foundation
+
+enum ForceDirection: Int {
+    case pi0,pi1, pi2, pi3, pi4, pi5, pi6, pi7
+}
