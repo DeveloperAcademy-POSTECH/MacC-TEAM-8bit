20db9e0011beb3032f67a7b17a15893ba9bb5bc4
Hyeonmin Kim <hminkim2@gmail.com>
2022-11-25 17:08:43 +0900

    [#227] feat - 네비게이션 바 사진 저장 버튼 생성

--- OrrRock/OrrRock/SaveRootFinding/RootFindingSaveViewController.swift
+++ OrrRock/OrrRock/SaveRootFinding/RootFindingSaveViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class RootFindingSaveViewController: UIViewController {
     
     private var goBackButton: UIBarButtonItem!
+    private var saveButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,15 +21,20 @@ class RootFindingSaveViewController: UIViewController {
     
     func setNavigationBar() {
         goBackButton = UIBarButtonItem(image: UIImage(systemName: "arrow.left"), style: .plain, target: self, action: #selector(goBackAction))
+        saveButton = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.down.fill"), style: .plain, target: self, action: #selector(saveAction))
         
         navigationController?.isToolbarHidden = false
         navigationController?.hidesBarsOnTap = true
         
         navigationItem.leftBarButtonItem = goBackButton
+        navigationItem.rightBarButtonItem = saveButton
     }
     
     @objc func goBackAction() {
         self.navigationController?.popViewController(animated: true)
     }
     
+    @objc func saveAction() {
+        // TODO: 사진 앱에 사진 저장하는 로직 추가
+    }
 }
