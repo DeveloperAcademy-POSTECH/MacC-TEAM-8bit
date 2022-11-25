b052b28a7e1048b10258e9a13555a32c40c06768
Hyeonmin Kim <hminkim2@gmail.com>
2022-11-25 16:35:49 +0900

    [#227] feat - RootFindingSaveViewController 파일 생성

---
+++ OrrRock/OrrRock/SaveRootFinding/RootFindingSaveViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RootFindingSaveViewController.swift
+//  OrrRock
+//
+//  Created by kimhyeongmin on 2022/11/25.
+//
+
+import UIKit
+
+class RootFindingSaveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
