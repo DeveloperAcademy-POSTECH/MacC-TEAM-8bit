186af5c9847a2fbf3133a3b24f8a904e20c9e2aa
sm-amoled <moktak072@naver.com>
2022-11-27 01:25:35 +0900

    [#225] fix - PR을 위한 SceneDelegate 수정

--- OrrRock/OrrRock/SceneDelegate.swift
+++ OrrRock/OrrRock/SceneDelegate.swift
@@ -19,8 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window?.backgroundColor = .black
         
-//                window?.rootViewController = UINavigationController(rootViewController: MainViewController())
-                window?.rootViewController = UINavigationController(rootViewController: RouteFindingFeatureViewController(routeInfo: RouteInfo(imageLocalIdentifier: "ImageLocalIdentifier", dataWrittenDate: Date(), gymName: "", problemLevel: 0, isChallengeComplete: false, pages: [PageInfo(rowOrder: 0)])))
+        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
+//                window?.rootViewController = UINavigationController(rootViewController: RouteFindingFeatureViewController(routeInfo: RouteInfo(imageLocalIdentifier: "ImageLocalIdentifier", dataWrittenDate: Date(), gymName: "", problemLevel: 0, isChallengeComplete: false, pages: [PageInfo(rowOrder: 0)])))
         window?.makeKeyAndVisible()
     }
     
