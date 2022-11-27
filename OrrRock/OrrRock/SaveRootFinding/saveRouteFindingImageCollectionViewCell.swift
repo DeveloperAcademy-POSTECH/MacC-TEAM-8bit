69fcf879c8563e4bb7cac3aa974b05198b42125d
Hyeonmin Kim <hminkim2@gmail.com>
2022-11-27 23:58:53 +0900

    [#227] feat - 커스텀 컬렉션뷰를 위한 컬렉션뷰 셀 생성

--- OrrRock/OrrRock/SaveRootFinding/saveRouteFindingImageCollectionViewCell.swift
+++ OrrRock/OrrRock/SaveRootFinding/saveRouteFindingImageCollectionViewCell.swift
@@ -1,12 +1,83 @@
 //
-//  saveRouteFindingImageCollectionViewCell.swift
+//  SaveRouteFindingImageCollectionViewCell.swift
 //  OrrRock
 //
 //  Created by kimhyeongmin on 2022/11/27.
 //
 
 import UIKit
+import SnapKit
 
-class saveRouteFindingImageCollectionViewCell: UICollectionViewCell {
+class SaveRouteFindingImageCollectionViewCell: UICollectionViewCell {
     
+    // MARK: Variables
+    
+    static let identifier: String = "SaveRouteFindingImageCollectionViewCell"
+    
+    var indexPathOfCell: IndexPath!
+    
+    let collectionViewCellwidth: Int = 58
+    
+    // MARK: View Components
+    
+    lazy var pageImage: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private lazy var selectedBar: UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    // MARK: Life Cycle Functions
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        selectedBar.backgroundColor = .clear
+    }
+    
+    // MARK: Functions
+    
+    func showSelectedBar() {
+        UIView.animate(withDuration: 0.2) {
+            self.selectedBar.backgroundColor = .orrUPBlue
+        }
+    }
+    
+    func hideSelectedBar() {
+        UIView.animate(withDuration: 0.2) {
+            self.selectedBar.backgroundColor = .clear
+        }
+    }
+    
+    // MARK: Set Up Functions
+    
+    func setUpLayout() {
+        contentView.addSubview(pageImage)
+        pageImage.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        contentView.addSubview(selectedBar)
+        selectedBar.snp.makeConstraints {
+            $0.width.equalTo(collectionViewCellwidth)
+            $0.height.equalTo(4)
+            $0.top.equalTo(pageImage.snp.bottom).offset(OrrPd.pd8.rawValue)
+        }
+    }
 }
