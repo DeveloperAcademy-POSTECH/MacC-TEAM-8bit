6d45ea27b1b82d1e457a4eebf509435ba7ddaa2d
Hyeonmin Kim <hminkim2@gmail.com>
2022-11-29 20:45:08 +0900

    [#227] feat - 루트 파인딩 레벨 입력 UI 구현

---
+++ OrrRock/OrrRock/SaveRootFinding/RouteFindingLevelSaveViewController.swift
@@ -0,0 +1,108 @@
+//
+//  RouteFindingLevelSaveViewController.swift
+//  OrrRock
+//
+//  Created by kimhyeongmin on 2022/11/29.
+//
+
+import UIKit
+
+import SnapKit
+
+final class RouteFindingLevelSaveViewController: UIViewController {
+    
+    private var cards: [SwipeableCardVideoView?] = []
+    private var currentSelectedLevel = 0
+    
+    let gymNameLabel : UILabel = {
+        let label = UILabel()
+        label.text = "난이도를 선택해주세요"
+        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.textColor = .orrBlack
+        return label
+    }()
+    
+    private lazy var newLevelPickerView: NewLevelPickerView = {
+        let view = NewLevelPickerView()
+        view.pickerSelectValue = 0
+        view.delegate = self
+        view.customTitle = "슬라이드 해주세요"
+
+        return view
+    }()
+    
+    private lazy var levelButtonImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.down")
+        imageView.tintColor = .orrGray500
+        
+        return imageView
+    }()
+    
+    private lazy var levelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        
+        return stackView
+    }()
+    
+    private lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 3
+        
+        return stackView
+    }()
+    
+    private lazy var emptyVideoView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .orrGray300
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+        
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orrWhite
+        overrideUserInterfaceStyle = .dark
+
+        setUpLayout()
+    }
+}
+
+extension RouteFindingLevelSaveViewController: NewLevelPickerViewDelegate {
+    
+    func didLevelChanged(selectedLevel: Int) {
+        currentSelectedLevel = selectedLevel
+    }
+    
+}
+
+private extension RouteFindingLevelSaveViewController {
+    
+    func setUpLayout() {
+        
+        view.addSubview(gymNameLabel)
+        gymNameLabel.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(OrrPd.pd16.rawValue)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(OrrPd.pd72.rawValue)
+            $0.height.equalTo(26)
+        }
+        
+        view.addSubview(newLevelPickerView)
+        newLevelPickerView.snp.makeConstraints {
+            $0.leading.equalTo(view.snp.leading)
+            $0.trailing.equalTo(view.snp.trailing)
+            $0.height.equalTo(120)
+            $0.top.equalTo(gymNameLabel.snp.bottom).offset(OrrPd.pd36.rawValue)
+        }
+    }
+}
