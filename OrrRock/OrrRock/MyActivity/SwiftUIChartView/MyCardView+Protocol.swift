12202d82491ea3281a0e667159cb3c4cccfe91ee
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-12-08 20:55:58 +0900

    [#394] feat - add protocol

---
+++ OrrRock/OrrRock/MyActivity/SwiftUIChartView/MyCardView+Protocol.swift
@@ -0,0 +1,16 @@
+//
+//  MyCardView+Protocol.swift
+//  OrrRock
+//
+//  Created by dohankim on 2022/12/08.
+//
+
+import UIKit
+
+protocol MyCardViewDelegate {
+    func longPressedCardView()
+}
+
+extension MyCardView {
+    
+}
--- OrrRock/OrrRock/MyActivity/SwiftUIChartView/MyCardView.swift
+++ OrrRock/OrrRock/MyActivity/SwiftUIChartView/MyCardView.swift
@@ -12,6 +12,8 @@ struct MyCardView: View {
     let highestLevel: Int
     let homeGymName: String
     
+    var delegate: MyCardViewDelegate?
+    
     var body: some View {
         ZStack(alignment: .center) {
             if highestLevel >= 0 {
@@ -82,26 +84,9 @@ struct MyCardView: View {
             .onEnded { _ in
                 DispatchQueue.main.asyncAfter(deadline: .now()) {
                     HapticManager.instance.impact(style: .heavy)
+                    delegate?.longPressedCardView()
                 }
             })
     }
     
-    func takeCapture() -> UIImage {
-        var image: UIImage?
-        guard let currentLayer = UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.layer else { return UIImage() }
-        
-        let currentScale = UIScreen.main.scale
-        
-        UIGraphicsBeginImageContextWithOptions(currentLayer.frame.size, false, currentScale)
-        
-        guard let currentContext = UIGraphicsGetCurrentContext() else { return UIImage() }
-        
-        currentLayer.render(in: currentContext)
-        
-        image = UIGraphicsGetImageFromCurrentImageContext()
-        
-        UIGraphicsEndImageContext()
-        
-        return image ?? UIImage()
-    }
 }
