fb524c9424f547ae925521f3b9191a9dd8c980c3
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-12-08 20:37:38 +0900

    [#394] feat - add take capture

--- OrrRock/OrrRock/MyActivity/SwiftUIChartView/MyCardView.swift
+++ OrrRock/OrrRock/MyActivity/SwiftUIChartView/MyCardView.swift
@@ -85,4 +85,23 @@ struct MyCardView: View {
                 }
             })
     }
+    
+    func takeCapture() -> UIImage {
+        var image: UIImage?
+        guard let currentLayer = UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.layer else { return UIImage() }
+        
+        let currentScale = UIScreen.main.scale
+        
+        UIGraphicsBeginImageContextWithOptions(currentLayer.frame.size, false, currentScale)
+        
+        guard let currentContext = UIGraphicsGetCurrentContext() else { return UIImage() }
+        
+        currentLayer.render(in: currentContext)
+        
+        image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return image ?? UIImage()
+    }
 }
